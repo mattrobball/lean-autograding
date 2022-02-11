c004f4539968eaa5b3a42624308df9e9c9eeeca8
Matthew Ballard <matthewrobertballard@gmail.com>
2022-02-11 12:30:57 -0500

    reworking the lean 3 test files

---
+++ .test/basic_test.lean
@@ -0,0 +1,12 @@
+import assignment
+
+theorem check : ∀ (x : Type), x = x :=
+begin
+  exact exercise, 
+end
+
+theorem check' : 0 = 1 := 
+begin
+  exact another_exercise, 
+end
+
--- src/assignment.lean
+++ src/assignment.lean
@@ -1,5 +1,12 @@
 variables x : Type 
 
+constant duh : ℕ
+constant doh : ℕ 
+constant dah : ℕ 
+
 lemma exercise : x = x := by refl  
 
 lemma another_exercise : 0 = 1 := sorry
+
+lemma yet_another_exercise: ℕ × ℕ × ℕ := ⟨ duh, doh, dah ⟩
+
